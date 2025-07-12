3faef85729b77e672b120c3febb966733d95ce5e
alifathi <alifathi8008@gmail.com>
2025-07-12 17:35:52 +0330

    changing model nodes to the class

--- models/model_nodes.py
+++ models/model_nodes.py
@@ -3,13 +3,17 @@ from models.config import MODEL_NAME, SYSTEM_PROMPT
 from langchain_core.messages import SystemMessage, AIMessage
 from utils.dicts import AgentState
 
-def build_model(model_name: str):
-    ''' A function to define the LM '''
-    return  OllamaLLM(model = model_name)
 
-def call_model(state: AgentState) -> AgentState:
-    ''' A node to call model '''
-    system_message = SystemMessage(content=SYSTEM_PROMPT)
-    response = model.invoke([system_message]+state['messages'])
-    state['messages'].append(response)
-    return state
+class model_nodes:
+    def __init__(self):
+        self.model = self.build_model(MODEL_NAME)
+    def call_model(self, state: AgentState) -> AgentState:
+        ''' A node to call model '''
+        system_message = SystemMessage(content=SYSTEM_PROMPT)
+        response = self.model.invoke([system_message]+state['messages'])
+        state['messages'].append(response)
+        return state
+    @staticmethod
+    def build_model(model_name: str):
+        ''' A function to define the LM '''
+        return  OllamaLLM(model = model_name)
