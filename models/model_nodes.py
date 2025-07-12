2810bcab68e7912be2e04fc5d5a0513c29d05695
alifathi <alifathi8008@gmail.com>
2025-07-12 15:41:10 +0330

    build_model developed to define the model; call_model node defined for a simple model call node

--- models/model_nodes.py
+++ models/model_nodes.py
@@ -1,3 +1,15 @@
 from langchain_ollama import OllamaLLM
 from models.config import MODEL_NAME, SYSTEM_PROMPT
-from utils
+from langchain_core.messages import SystemMessage, AIMessage
+from utils.dicts import AgentState
+
+def build_model(model_name: str):
+    ''' A function to define the LM '''
+    return  OllamaLLM(model = model_name)
+
+def call_model(state: AgentState) -> AgentState:
+    ''' A node to call model '''
+    system_message = SystemMessage(content=SYSTEM_PROMPT)
+    response = model.invoke([system_message]+state['messages'])
+    state['messages'].append(response)
+    return state
