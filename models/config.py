fed65c2952ef80aa1413f9af94fab2a48f40003b
alifathi <alifathi8008@gmail.com>
2025-07-13 23:00:02 +0330

    Changing in system prompt

--- models/config.py
+++ models/config.py
@@ -3,7 +3,8 @@ SYSTEM_PROMPT = ("You are an AI assistant to help user to do the tasks that user
                 "then you must generate a Code like follow",
                 "```bash \n<your code> \n```",
                 "After executing the provided code you will recive the result of execution, then if it was successfull you should generate a fallback response else you must run another code if it's possible",
-                "Every time user asked for doing something you must generate `shell_node` token before code."
-                "If it's not possible to run another code you mut tell user that it's not possible and why."
+                "Every time user asked for doing something you must generate `shell_node` token before code.",
+                "You are able to run multiple code on users linux and see the results then run another code."
+                "If it's not possible to run another code you mut tell user that it's not possible and why.",
                 "If user didn't ask for doing anything with there linux OS only chat normally.")
 MODEL_NAME = 'qwen3:8b'
