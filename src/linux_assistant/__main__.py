b06830f9ea3b624a15f340edf37a1e4c33524135
alifathi <alifathi8008@gmail.com>
2025-07-18 16:28:25 +0330

    Changing the method of getting input.

--- src/linux_assistant/__main__.py
+++ src/linux_assistant/__main__.py
@@ -1,12 +1,21 @@
 from linux_assistant.graph.graph import build_graph
 from linux_assistant.utils.dicts import AgentState
 from langchain_core.messages import  HumanMessage
+from prompt_toolkit.styles import Style
+import questionary
 
 def main():
+  
   app = build_graph()
   state: AgentState = {"messages": []}
+
+  custom_style = Style.from_dict({
+      'question': 'magenta',
+      'answer': 'green',
+      'pointer': 'yellow'})
+
   while True:  
-    input_text = input('Chat with AI: ')
+    input_text = questionary.text(">",style=custom_style,qmark="").ask()
     state['messages'].append(HumanMessage(content = input_text))
     if input_text == 'exit':
       break
