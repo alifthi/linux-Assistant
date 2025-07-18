ba6a68d6e07406241e4c0a6906d274414a273c93
alifathi <alifathi8008@gmail.com>
2025-07-18 17:30:08 +0330

    console_utils added for input output texts.

---
+++ src/linux_assistant/utils/console_utils.py
@@ -0,0 +1,22 @@
+from pyfiglet import Figlet
+from rich.console import Console
+from rich.markdown import Markdown
+from prompt_toolkit.styles import Style
+import questionary
+
+class console_utils:
+    def __init__(self):
+        self.console = Console()
+        self.banner = Figlet(font="slant").renderText("Linux Assistant")
+        self.intro_md = Markdown("Welcome! Enjoy your linux more.")
+        self.custom_style = Style.from_dict({
+            'question': 'magenta',
+            'answer': 'gray',
+            'pointer': 'yellow'})
+    def release_banner(self):
+        self.console.print(self.banner, style="cyan")
+        self.console.print(self.intro_md)
+    
+    def get_user_input(self):
+        cmd = questionary.text(">",style=self.custom_style,qmark="").ask()
+        return cmd        
